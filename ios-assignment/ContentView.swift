72c7afc85ac40489978c1535e7886cdfdcff2d05
Dongyoon Kim <dy128.kim@wisebirds.com>
2025-09-17 19:24:24 +0900

    Initial Commit

---
+++ ios-assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-assignment
+//
+//  Created by wisebirds on 9/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-assignment/ios_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_assignmentApp.swift
+//  ios-assignment
+//
+//  Created by wisebirds on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
